6dbd6c9ea8b73342d0ee6dfe6e5854bf126e788f
kafkaqin <kafkaqin@gmail.com>
2024-10-26 08:16:08 +0800

    first commit

--- cmd/kube-scheduler/scheduler.go
+++ cmd/kube-scheduler/scheduler.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/kafkaqin/kube-scheduler/cmd/kube-scheduler/app"
 	"k8s.io/component-base/cli"
 	_ "k8s.io/component-base/logs/json/register" // for JSON log format registration
 	_ "k8s.io/component-base/metrics/prometheus/clientgo"
 	_ "k8s.io/component-base/metrics/prometheus/version" // for version metric registration
+	"kube-scheduler/cmd/kube-scheduler/app"
 	"os"
 )
 
